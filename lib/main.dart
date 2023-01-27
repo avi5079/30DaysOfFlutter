faa5d705490b923c9dc7a47a4adf7a23720936c0
avi5079 <aviral.srivastava2102@gmail.com>
2023-01-27 12:08:06 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'pages/home_page.dart';
 
@@ -17,14 +17,8 @@ class MyApp extends StatelessWidget {
       // home: const HomePage(),
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primaryColor: Colors.deepPurple,
-          primarySwatch: Colors.deepOrange,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme()),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => const LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog App"),
+        // backgroundColor: Colors.white,
+        // elevation: 0.0,
+        // iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Center(
         child: Text("Welcome to $days days of flutter by $name"),
